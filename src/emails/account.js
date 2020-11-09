12e672eae0feee7394e7fa25e798183f3dd0209c
Magdalena Senger <magdas@localhost.localdomain>
2020-11-09 14:48:33 +0100

    restart project

--- src/emails/account.js
+++ src/emails/account.js
@@ -3,33 +3,29 @@ const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, nameFirst, rekoDate) => {
-    sgMail.send(
-        {
-            to: email,
-            from: {
-                email: 'dworek.rekolekcje.dziewczyny@gmail.com',
-                name: 'Rekolekcje w Dworku'
-            },
-            subject: 'Potwierdzenie zapisu na rekolekcje w Dworku',
-            html: `<p>Cześć ${nameFirst},</p>
-                    <p>właśnie zapisałaś się na rekolekcje w Dworku w terminie ${rekoDate}.</p>
-                    <p>Koszt rekolekcji dla studentek to 270 zł, a dla licealistek 190 zł.</p>
-                    <p>Rekolekcje zaczynają się o godz. 20:00 wspólną kolacją.</p>
-                    <p>Konto do wpłat:</p>
-                    <p>Podlaskie Towarzystwo Oświatowo-Kulturalne (PTOK)</p>
-                    <p>Boża Wola 15, 05-332 Siennica</p>
-                    <p>Nr rachunku: 37 9226 0005 0045 0700 2000 0010</p>
-                    <p>Bank: Spółdzieczy w Mińsku Mazowieckim, Oddział w Siennicy</p>
-                    <p>Tytuł wpłaty: za pobyt w Centrum Konferencyjnym i Formacyjnym "Dworek" w dniach ... oraz na cele statutowe</p>
-                    <p>W razie pytań, odpisz na tego maila lub zadzwoń: 539 818 750 (Magda)</p>
-                    <p><b>Do zobaczenia! :)</b></p>`            
-            // bcc: {
-            //             email: 'dworek.rekolekcje.dziewczyny@gmail.com',
-            //             name: 'Rekolekcje w Dworku',
-            //             html: `test ${rekoDate} oraz ${nameFirst}`
-            //     }
-        }   
-        )
+    sgMail.send({
+        to: email,
+        from: {
+            email: 'dworek.rekolekcje.dziewczyny@gmail.com',
+            name: 'Rekolekcje w Dworku'
+        },
+        subject: 'Potwierdzenie zapisu na rekolekcje w Dworku',
+        html: `<p>Cześć ${nameFirst},</p>
+        <p>właśnie zapisałaś się na rekolekcje w Dworku w terminie ${rekoDate}.</p>
+        <p>Koszt rekolekcji dla studentek to 270 zł, a dla licealistek 190 zł.</p>
+        <p>Konto do wpłat:</p>
+        <p>Podlaskie Towarzystwo Oświatowo-Kulturalne (PTOK)</p>
+        <p>Boża Wola 15, 05-332 Siennica</p>
+        <p>Nr rachunku: 37 9226 0005 0045 0700 2000 0010</p>
+        <p>Bank: Spółdzieczy w Mińsku Mazowieckim, Oddział w Siennicy</p>
+        <p>Tytuł wpłaty: za pobyt w Centrum Konferencyjnym i Formacyjnym "Dworek" w dniach ... oraz na cele statutowe</p>
+        <p>W razie pytań, odpisz na tego maila lub zadzwoń: 539 818 750 (Magda)</p>
+        <p><b>Do zobaczenia! :)</b></p>`,
+        bcc: {
+            email: 'dworek.rekolekcje.dziewczyny@gmail.com',
+            name: 'Rekolekcje w Dworku'
+        }
+    })
 }
 
 module.exports = {
