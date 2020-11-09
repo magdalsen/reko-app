ce00e790487603d57080debdab5d6eced6577143
Magdalena Senger <magdas@localhost.localdomain>
2020-11-09 14:52:09 +0100

    restore2

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ app.get('/payments', (req, res) => {
 })
 
 app.get('/form', (req, res) => {
-    if (!req.query.nameFirst || !req.query.surname || !req.query.city || !req.query.rekoDate || !req.query.age || !req.query.tel || !req.query.email) {
+    if (!req.query.nameFirst || !req.query.surname || !req.query.rekoDate || !req.query.age || !req.query.tel || !req.query.email) {
         return res.send({
             error: 'Wszystkie pola (oprócz "Uwagi") muszą być uzupełnione!'
         })
@@ -62,14 +62,13 @@ app.get('/form', (req, res) => {
     }
     nameFirst = req.query.nameFirst
     surname = req.query.surname
-    city = req.query.city
     rekoDate = req.query.rekoDate
     age = req.query.age
     tel = req.query.tel
     email = req.query.email
     arrive = req.query.arrive
     comments = req.query.comments
-    const user = new User({ nameFirst, surname, city, rekoDate, age, tel, email, arrive, comments })
+    const user = new User({ nameFirst, surname, rekoDate, age, tel, email, arrive, comments })
     user.save().then(() => {
         res.send(user)
         sendWelcomeEmail(user.email, user.nameFirst, user.rekoDate)
